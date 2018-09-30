ccd82cc6f09d07ff5a77b5cb2499b8912aa0ce4b
cyril <cyril_fautrez@hotmail.com>
2018-09-30 21:22:56 +0200

    add integer initialisation

--- app/src/main/java/com/example/cyril/myapplication/MainActivity.java
+++ app/src/main/java/com/example/cyril/myapplication/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         //comment and comment in android studio
+
+        int i = 0;
     }
 }
